167fea13a292b2e517e8984e96a437cde055b422
James Lewellyn <Lewellyn.James@gmail.com>
2023-04-07 19:59:59 -0500

    v1

---
+++ python_poetry_template/some_executable.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Contains the main package functionality."""
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def main() -> None:
+    """Do the main thing."""
+    # Get arguments
+    logging.info("some-executable action!")
