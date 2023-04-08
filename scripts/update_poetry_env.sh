dae696f108f75546897bebd8610306b343e7acc5
James Lewellyn <Lewellyn.James@gmail.com>
2023-04-07 22:02:30 -0500

    basic poetry package template

---
+++ scripts/update_poetry_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+THIS_SCRIPT="$(readlink -f "${BASH_SOURCE[0]}")"
+THIS_SCRIPT_DIR="$(dirname "$THIS_SCRIPT")"
+PYTHON_REPO_DIR="$(dirname "$THIS_SCRIPT_DIR")"
+PYTHON_SOURCE_DIR="${PYTHON_REPO_DIR}/$(basename "$PYTHON_REPO_DIR")"
+
+(cd "$PYTHON_REPO_DIR"
+	python3 -m poetry update
+)
